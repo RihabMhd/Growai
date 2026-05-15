5a6b1126d7c5f9cc6db0b41dcff742b3cc9f9d6d
RihabMhd <rihabmahdi19@gmail.com>
2026-05-15 10:36:35 +0100

    feat: added the necessary files and will start with auth for this branch

---
+++ routes/api.php
@@ -0,0 +1,11 @@
+<?php 
+
+// Route::prefix('auth')->group(function () {
+//     Route::post('/login', ...);
+//     Route::post('/logout', ...);
+// });
+
+// Route::apiResource('orders', OrderController::class);
+// Route::apiResource('clients', ClientController::class);
+// Route::apiResource('products', ProductController::class);
+// Route::apiResource('shops', ShopController::class);
